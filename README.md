775c4e4c94f43724ff63fa9ac4455e1d6006357a
Thomas LEBEAU <thomas.lebeau@g-nova.fr>
2013-12-17 16:49:01 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Menu Accessible Vanilla Js
+
+[menu en live](http://thomaslebeau.github.io/menuAccessibleJs)
---
+++ menu.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  document.querySelector("html").className = 'js';
+
+  var menu = document.getElementById('menu'),
+      liMenu = document.querySelectorAll('#menu > li');
+
+  [].forEach.call(liMenu, function(el) {
+    el.addEventListener('focusin', function() {
+      this.querySelector('ul').style.top = '55px';
+    })
+    el.addEventListener('focusout', function() {
+      this.querySelector('ul').style.top = '-9999px';
+    })
+  })
+});
